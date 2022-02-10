59294050572a7ebfaf71d1bdc6d59ec69760cb34
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-02-10 11:05:45 +0000

    src: refactor greet, update -main doc-string

--- src/practicalli/clojure_app_template.clj
+++ src/practicalli/clojure_app_template.clj
@@ -3,10 +3,21 @@
 
 (defn greet
   "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+  [moniker]
+  (println (str "Welcome to Clojure " (or (:name moniker) "everyone"))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Entry point into the application via clojure.main -M"
   [& args]
   (greet {:name (first args)}))
+
+
+
+;; Rich comment block with redefined vars ignored
+#_{:clj-kondo/ignore [:redefined-var]}
+(comment
+
+  (-main)
+  (-main "Jenny")
+
+  #_()) ;; End of rich comment block
