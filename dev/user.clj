c6613abcd65ee042a8b61e5830faa297f4bc8f14
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-03-27 17:29:47 +0100

    dev: add portal and mulog to REPL start-up
    - open portal with gruvbox theme
    - add portal as tap source
    - set common values for all mulog events
    - start mulog custom tap publisher
    - find libraries example code

--- dev/user.clj
+++ dev/user.clj
@@ -1,44 +1,90 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Tools for REPL Driven Development
+;; ---------------------------------------------------------
+;; REPL workflow development tools
 ;;
-;; Start repl with aliases from practicalli/clojure-deps-edn
-;; :lib/hotload:inspect/portal-cli
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Include development tool libraries vai aliases from practicalli/clojure-cli-config
+;; Start Rich Terminal UI REPL prompt
+;; `clojure -M:repl/reloaded`
+;;
+;; ---------------------------------------------------------
 
 
 (ns user
-  "Tools for REPL Driven Development")
+  "Tools for REPL Driven Development"
+  (:require
+   [clojure.tools.deps.alpha.repl :refer [add-libs]]
+   [find-deps.core :as find-lib]
+   [portal.api :as inspect]
+
+   [com.brunobonacci.mulog :as mulog]
+   [mulog-publisher] ; tap mulog events
+   ;; [system] ; Component management
+   ))
 
 
+;; ---------------------------------------------------------
+;; Start Portal and capture all evaluation results
+
+;; Open Portal window in browser with dark theme
+;; https://cljdoc.org/d/djblue/portal/0.37.1/doc/ui-concepts/themes
+(inspect/open {:portal.colors/theme :portal.colors/gruvbox})
+;; (inspect/open {:portal.colors/theme :portal.colors/solarized-light})
+
+;; Add portal as a tap> target
+(add-tap #'portal.api/submit)
+
+;; ---------------------------------------------------------
+
+
+;; ---------------------------------------------------------
+;; Mulog events and publishing
+
+;; set event global context - information added to every event for REPL workflow
+(mulog/set-global-context! {:app-name "Practicalli Service",
+                            :version "0.1.0", :env "dev"})
+
+(def mulog-tap-publisher
+  "Start mulog custom tap publisher to send all events to Portal
+  and other tap sources
+  `mulog-tap-publisher` to stop publisher"
+  (mulog/start-publisher!
+    {:type :custom, :fqn-function "mulog-publisher/tap"}))
+
+(mulog/log ::emacs-event ::ns (ns-publics *ns*))
+
+#_mulog-tap-publisher
+
+;; ---------------------------------------------------------
+
+
+;; ---------------------------------------------------------
+;; Find Libraries
+(comment
+  (find-lib/deps "library-name") ; fuzzy library search
+  (find-lib/print-deps "library-name")) ; show results as table
+;; ---------------------------------------------------------
+
+;; ---------------------------------------------------------
 ;; Hotload libraries into running REPL
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Start REPL with :lib/hotload alias from practicalli/clojure-deps-edn
+;; `deps-*` LSP snippets to add dependency forms
 (comment
-  (require '[clojure.tools.deps.alpha.repl :refer [add-libs]])
   (add-libs '{domain/library-name {:mvn/version "1.0.0"}})
-  #_()) ;; End of rich comment block
+  #_()) ; End of rich comment
+;; ---------------------------------------------------------
 
-
-;; Portal Data Inspector -
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Start REPL with :lib/hotload alias from practicalli/clojure-deps-edn
+;; ---------------------------------------------------------
+;; Portal Data Inspector
 (comment
-  (require '[portal.api :as inspect])
   ;; Open a portal inspector in browser window - light theme
-  (inspect/open {:portal.colors/theme :portal.colors/solarized-light})
-  ;; Open Portal window in browser with dark theme
-  (inspect/open {:portal.colors/theme :portal.colors/solarized-dark})
+  ;; (inspect/open {:portal.colors/theme :portal.colors/solarized-light})
 
-  ;; Add portal as a tap> target
-  (inspect/tap)
+  (inspect/clear) ; Clear all values in portal window (allows garbage collection)
 
-  ;; Clear all values in the portal inspector window
-  (inspect/clear)
+  (remove-tap #'inspect/submit) ; Remove portal from `tap>` sources
 
-  ;; Close the portal window
-  (inspect/close)
+  (inspect/close) ; Close the portal window
 
-  #_()) ;; End of rich comment
+  (inspect/docs) ; View docs locally via Portal
 
+  #_()) ; End of rich comment
 
-;; TODO: Component services - Integrant-repl
+;; ---------------------------------------------------------
